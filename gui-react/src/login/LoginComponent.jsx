4d59592c0402fb0eaa99b929af3fc1cb7ef3490d
Ivan Uribe <ivanmauriciour@ufps.edu.co>
2021-11-20 21:45:33 -0500

    Integración de la gui en react

---
+++ gui-react/src/login/LoginComponent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class LoginComponent extends Component {
+
+    render(){
+        return(
+            <div className="loginComponent">
+                <img src="" alt="" />
+            </div>
+        );
+    }
+
+   
+}
+
+export default LoginComponent;
