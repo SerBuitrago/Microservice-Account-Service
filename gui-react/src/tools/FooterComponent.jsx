8e5cd5670c2f1472c612cf920d22ed95ae69dc9a
Danielcaos <danielcaos@ufps.edu.co>
2021-11-20 22:17:18 -0500

    creando footer

---
+++ gui-react/src/tools/FooterComponent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class FooterComponent extends Component {
+
+    render(){
+        return(
+            <div className="FooterComponent">
+                Copyright © Universidad Francisco de Paula Santander 2021
+            </div>
+        );
+    }
+
+   
+}
+
+export default FooterComponent;
