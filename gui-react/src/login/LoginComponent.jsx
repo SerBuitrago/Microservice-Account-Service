3629b8243ba7571c69063500b6c30e69f92861e8
soleimygomez <danielagbaron@gmail.com>
2021-11-25 20:02:06 -0500

    implementando vista del login

--- gui-react/src/login/LoginComponent.jsx
+++ gui-react/src/login/LoginComponent.jsx
@@ -1,31 +1,36 @@
-import React, { Component } from 'react'
-import '../css/login.css';
-
+import React, { Component } from "react";
+import "../css/login.css";
+import logo from "./../ufps_resource/3.png";
 
 class LoginComponent extends Component {
-
-    render(){
-        return(
+    render() {
+        return (
             <div className="loginComponent">
-               
+                <div className="UFPS">
+                    <p className="bienvenido">Bienvenido al Portal.</p>
+                </div>
 
-               <img src="" alt="" />
+                <form>
+                    <input
+                        className="campos"
+                        type="text"
+                        placeholder="Código"
+                    />
+                    <br />
+                    <input
+                        className="campos"
+                        type="text"
+                        placeholder="Contraseña"
+                    />
 
-               <p>Bienvenido a portal.</p>
-
-
-               <input type="text" placeholder="Código"/>
-               <br />
-               <input type="text" placeholder="Contraseña"/>
-
-               <button>Acceder</button>
-
-               
+                    <button>Acceder</button>
+                </form>
+                {/* <div>
+                <input type="checkbox">Recuerdame</input>
+                </div> */}
             </div>
         );
     }
-
-   
 }
 
 export default LoginComponent;
