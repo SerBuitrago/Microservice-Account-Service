9727b7eb6172ba4a146a232a164e6d8c345de2be
Ivan Uribe <ivanmauriciour@ufps.edu.co>
2021-11-30 08:25:47 -0500

    Control creado

---
+++ gui-react/src/control/ControlComponente.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class ControlComponent extends Component {
+
+    render(){
+        return(
+            <div className="controlComponent">
+
+            </div>
+        );
+    }
+}
+
+export default ControlComponent;
