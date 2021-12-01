e76d0484e66aa55a6fa5dfaf4d5a5cf5c1d8eaef
Ivan Uribe <ivanmauriciour@ufps.edu.co>
2021-12-01 08:54:42 -0500

    Mas

--- gui-react/src/control/SidebarComponent.jsx
+++ gui-react/src/control/SidebarComponent.jsx
@@ -1,23 +1,47 @@
+import { Button, ListItem } from "@material-ui/core";
 import { Component } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import classes from '../css/control.module.css';
 
-class SidebarComponent extends Component{
-    render(){
-        return(
+
+
+class SidebarComponent extends Component {
+
+
+
+    render() {
+        return (
             <div className={classes.sidebar}>
-            
-                <ul>
-                    <li>
-                        <Link to="/"> Cuentas </Link>
-                    </li>
-                    <li>
-                        <Link to="/usuarios"> Usuarios </Link>
-                    </li>
-                    <li>
-                        <Link to="/permisos"> Permisos </Link>
-                    </li>
-                </ul>
+
+                <ListItem component="nav">
+                    <Button variant="contained" color="error" >
+                        Cuentas
+                    </Button>
+
+                    <Link to="/">
+                        <Button variant="contained" color="success" >
+                            Cuentas
+                        </Button>
+                    </Link>
+
+
+                    <Link to="/usuarios">
+                        <Button variant="contained" color="success" >
+                            Usuarios
+                        </Button>
+                    </Link>
+
+
+
+                    <Link to="/permisos">
+                        <Button variant="contained" color="success" >
+                            Permisos
+                        </Button>
+                    </Link>
+
+                </ListItem>
+
+              
 
             </div>
         );
