9a6c94919f07ad1743ea342a3df32970543ac6c6
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-12-14 11:50:16 -0500

    Add microservices -> services

--- config/services.php
+++ config/services.php
@@ -9,6 +9,39 @@ return [
     'microservices' => [
         'example' => [
             'base_uri' => 'http://localhost:8100/api',
+        ],
+        'user' => [
+            'base_uri' => env('HOST_MICROSERVICE_USER'),
+            "services" => [
+                "follow" => env('HOST_MICROSERVICE_USER')."/follow",
+                "user" => env('HOST_MICROSERVICE_USER')."/user",
+                "student" => env('HOST_MICROSERVICE_USER')."/student",
+            ],
+            "token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+        ],
+        'tutoring' => [
+            'base_uri' => env('HOST_MICROSERVICE_TUTORIN'),
+            "token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        ],
+        'chat' => [
+            'base_uri' => env('HOST_MICROSERVICE_CHAT'),
+            "token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        ],
+        'knowledge' => [
+            'base_uri' => env('HOST_MICROSERVICE_KNOWLEDGE'),
+            "token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        ],
+        'audit' => [
+            'base_uri' => env('HOST_MICROSERVICE_AUDIT'),
+            "token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        ],
+        'notification' => [
+            'base_uri' => env('HOST_MICROSERVICE_NOTIFICATIONS'),
+            "token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        ],
+        'report' => [
+            'base_uri' => env('HOST_MICROSERVICE_REPORTS'),
+            "token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
         ]
     ]
 ];
