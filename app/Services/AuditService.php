34dca91ac6d47945bb684f9930fbd097404739a1
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-12-17 15:59:03 -0500

    Enpoint Service Audit and Tutoring

--- app/Services/AuditService.php
+++ app/Services/AuditService.php
@@ -16,4 +16,29 @@ class AuditService
         $this->baseUri = config('services.microservices.audit.base_uri');
         $this->token = config('services.microservices.audit.base_uri.token');
     }
+
+    public function fetchReadAll()
+    {
+        return $this->request('GET', '/');
+    }
+
+    public function fetchRead($id)
+    {
+        return $this->request('GET', "/{$id}");
+    }
+
+    public function create($data)
+    {
+        return $this->request('POST', '/', $data);
+    }
+
+    public function update($id, $data)
+    {
+        return $this->request('PUT', "/{$id}", $data);
+    }
+
+    public function delete($id)
+    {
+        return $this->request('DELETE', "/{$id}");
+    }
 }
--- app/Services/TutoringService.php
+++ app/Services/TutoringService.php
@@ -16,4 +16,77 @@ class TutoringService
         $this->baseUri = config('services.microservices.tutoring.base_uri');
         $this->token = config('services.microservices.tutoring.base_uri.token');
     }
+
+    /**
+     * Tema
+     */
+    public function fetchReadTemaAll()
+    {
+        return $this->request('GET', '/tema/list');
+    }
+
+    public function fetchReadTemaNombre($nombre){
+        return $this->request('GET', "/tema/busquedaNombre/{$nombre}");
+    }
+
+    public function createTema($data)
+    {
+        return $this->request('POST', '/tema/save', $data);
+    }
+
+    public function updateTema($data)
+    {
+        return $this->request('PUT', "/tema", $data);
+    }
+
+    public function deleteTema($id, $nombre)
+    {
+        return $this->request('DELETE', "/tema/{$id}/{$nombre}");
+    }
+
+    /**
+     * Tutoria
+     */
+    public function fetchReadTutoriaNombre($nombre){
+        return $this->request('GET', "/tutoria/busquedaNombre/{$nombre}");
+    }
+
+    public function fetchReadTutoriaNotificacionesAll()
+    {
+        return $this->request('GET', '/tutoria/notificacionesall');
+    }
+
+    public function fetchReadTutoriaAll()
+    {
+        return $this->request('GET', '/tutoria/list');
+    }
+
+    public function fetchReadTutoriaActivasAll()
+    {
+        return $this->request('GET', '/tutoria/activas');
+    }
+
+    public function fetchReadTutoriaTerminadasAll()
+    {
+        return $this->request('GET', '/tutoria/terminadas');
+    }
+
+    public function fetchTutoriaInscribirse($id, $idusuario){
+        return $this->request('GET', "/tutoria/inscribirse/{$id}/{$idusuario}");
+    }
+
+    public function createTutoria($data)
+    {
+        return $this->request('POST', '/tutoria/save', $data);
+    }
+
+    public function updateTutoria($data)
+    {
+        return $this->request('PUT', "/tutoria", $data);
+    }
+
+    public function deleteTutoria($id, $nombre)
+    {
+        return $this->request('DELETE', "/tutoria/{$id}/{$nombre}");
+    }
 }
